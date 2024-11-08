241fbfabf67a45ab49f5cc8cdf6fba41aa8e2639
Sitt Guruvanich <aekazitt@gmail.com>
2024-11-08 12:54:47 +0700

    chore: version bump; 0.1.1 -> 0.1.2

--- src/acqua/types/validator.py
+++ src/acqua/types/validator.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2024 All rights reserved.
 # FILENAME:    ~~/src/acqua/types/validator.py
-# VERSION:     0.1.1
+# VERSION:     0.1.2
 # CREATED:     2024-11-07 14:00
 # AUTHOR:      Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
 # DESCRIPTION:
